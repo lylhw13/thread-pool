b9300154f806640269aa6211abaecaec2453dd63
lylhw13 <lylhw13@gmail.com>
2021-10-16 23:07:50 +0800

    add test for dynamic

---
+++ test/plot.py
@@ -0,0 +1,125 @@
+str = '''
+jobnum is 1, thread_num is 2
+jobnum is 10, thread_num is 3
+jobnum is 9, thread_num is 3
+jobnum is 8, thread_num is 3
+jobnum is 7, thread_num is 3
+jobnum is 6, thread_num is 3
+jobnum is 15, thread_num is 6
+jobnum is 14, thread_num is 6
+jobnum is 13, thread_num is 6
+jobnum is 12, thread_num is 6
+jobnum is 11, thread_num is 6
+jobnum is 10, thread_num is 6
+jobnum is 9, thread_num is 6
+jobnum is 28, thread_num is 9
+jobnum is 27, thread_num is 9
+jobnum is 26, thread_num is 9
+jobnum is 25, thread_num is 9
+jobnum is 24, thread_num is 9
+jobnum is 33, thread_num is 16
+jobnum is 32, thread_num is 16
+jobnum is 31, thread_num is 16
+jobnum is 30, thread_num is 16
+jobnum is 29, thread_num is 16
+jobnum is 28, thread_num is 16
+jobnum is 27, thread_num is 16
+jobnum is 26, thread_num is 16
+jobnum is 25, thread_num is 16
+jobnum is 24, thread_num is 16
+jobnum is 23, thread_num is 16
+jobnum is 22, thread_num is 16
+jobnum is 21, thread_num is 16
+jobnum is 20, thread_num is 16
+jobnum is 19, thread_num is 16
+jobnum is 28, thread_num is 16
+jobnum is 27, thread_num is 16
+jobnum is 26, thread_num is 16
+jobnum is 25, thread_num is 16
+jobnum is 24, thread_num is 16
+jobnum is 23, thread_num is 16
+jobnum is 22, thread_num is 16
+jobnum is 21, thread_num is 16
+jobnum is 20, thread_num is 16
+jobnum is 19, thread_num is 16
+jobnum is 18, thread_num is 16
+jobnum is 17, thread_num is 16
+jobnum is 16, thread_num is 16
+jobnum is 15, thread_num is 15
+jobnum is 14, thread_num is 14
+jobnum is 13, thread_num is 13
+jobnum is 12, thread_num is 12
+jobnum is 11, thread_num is 11
+jobnum is 10, thread_num is 10
+jobnum is 19, thread_num is 11
+jobnum is 18, thread_num is 11
+jobnum is 27, thread_num is 16
+jobnum is 26, thread_num is 16
+jobnum is 25, thread_num is 16
+jobnum is 24, thread_num is 16
+jobnum is 23, thread_num is 16
+jobnum is 22, thread_num is 16
+jobnum is 21, thread_num is 16
+jobnum is 20, thread_num is 16
+jobnum is 19, thread_num is 16
+jobnum is 18, thread_num is 16
+jobnum is 17, thread_num is 16
+jobnum is 16, thread_num is 16
+jobnum is 15, thread_num is 15
+jobnum is 14, thread_num is 14
+jobnum is 13, thread_num is 14
+jobnum is 22, thread_num is 14
+jobnum is 21, thread_num is 14
+jobnum is 29, thread_num is 16
+jobnum is 28, thread_num is 16
+jobnum is 27, thread_num is 16
+jobnum is 26, thread_num is 16
+jobnum is 25, thread_num is 16
+jobnum is 24, thread_num is 16
+jobnum is 23, thread_num is 16
+jobnum is 22, thread_num is 16
+jobnum is 21, thread_num is 16
+jobnum is 20, thread_num is 16
+jobnum is 19, thread_num is 16
+jobnum is 18, thread_num is 16
+jobnum is 17, thread_num is 16
+jobnum is 16, thread_num is 16
+jobnum is 15, thread_num is 15
+jobnum is 14, thread_num is 14
+jobnum is 13, thread_num is 14
+jobnum is 12, thread_num is 13
+jobnum is 11, thread_num is 11
+jobnum is 10, thread_num is 10
+jobnum is 9, thread_num is 9
+jobnum is 8, thread_num is 8
+jobnum is 7, thread_num is 7
+jobnum is 6, thread_num is 6
+jobnum is 5, thread_num is 5
+jobnum is 4, thread_num is 4
+jobnum is 3, thread_num is 3
+jobnum is 2, thread_num is 2
+jobnum is 1, thread_num is 2
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    jobnum = []
+    threadnum = []
+    for line in str.split('\n'):
+        if line:
+            data = line.replace(',', ' ').split()
+            jobnum.append(int(data[2]))
+            threadnum.append(int(data[-1]))
+
+    t = np.arange(0, len(jobnum), 1)
+    fig, ax = plt.subplots()
+    ax.plot(t, jobnum, label="job num")
+    ax.plot(t, threadnum, label="thread num")
+    ax.legend()
+    ax.grid()
+    fig.savefig("dynamic.png")
+    plt.show()
+
+    print("hello")
---
+++ test/test.c
@@ -0,0 +1,68 @@
+#include "thread-pool.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void function(void *i)
+{
+    pthread_t thr;
+    thr = pthread_self();
+    LOGD("id %ld print %d\n", (long)thr, *(int*)i);
+    sleep(random() % 30);
+}
+
+void test_dynamic()
+{
+    int i;
+    threadpool_t *tp;
+    tp = threadpool_init(0, dynamic_num);
+    for (i = 0; i< 100; ++i) {
+        int* j = malloc(sizeof(int));
+        job_t *job = (job_t *)malloc(sizeof(job_t));
+        job->jobfun = function;
+        *j = i;
+        job->args = j;
+        threadpool_add_job(tp, job);
+        if (i % 10 == 0)
+            sleep(random() % 30);
+    }
+
+    LOGD("sleep\n");
+    while(tp->jobsnum > 0)
+        ;
+    threadpool_destory(tp, shutdown_waitall);
+    return;
+}
+
+void test_fix()
+{
+    int i;
+    threadpool_t *tp;
+    int workernum = 0;
+    tp = threadpool_init(workernum, fix_num);
+    for (i = 0; i< 100; ++i) {
+        int* j = malloc(sizeof(int));
+        job_t *job = (job_t *)malloc(sizeof(job_t));
+        job->jobfun = function;
+        *j = i;
+        job->args = j;
+        threadpool_add_job(tp, job);
+        sleep(random() % 3);
+        if (i > 1 && i % 10 == 0) {
+            threadpool_change_target_workernum(tp, random() % 10);
+        }
+    }
+
+    LOGD("sleep\n");
+    while(tp->jobsnum > 0)
+        ;
+    threadpool_destory(tp, shutdown_waitall);
+    return;
+}
+
+int main()
+{
+    test_dynamic();
+    // test_fix();
+    return 0;
+}
